2b70f20b7cf599957e7a0f6bce9b36db0c8dfcb0
Ahmet <ahmetkhrmn7654321@gmail.com>
2024-02-29 19:26:12 +0300

    merhaba

--- src/P05.java
+++ src/P05.java
@@ -1,7 +1,7 @@
 public class P05 {
     public static void main(String[] args) {
 
-        System.out.println("Merhaba v2.5");
+        System.out.println("Merhaba v2.5 ");
 
         System.out.println("Merhaba ben T135");
     }
